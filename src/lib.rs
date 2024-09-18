2814c6390ea086e80920c9a69d5340b1da25e35c
Divyansh <divyansh.nankani@gmail.com>
2024-09-18 09:16:02 +0530

    a clean version, removed todo's and notes

--- src/lib.rs
+++ src/lib.rs
@@ -18,15 +18,11 @@ mod tests {
         assert_eq!(result, 4);
     }
 
-    // TODO: write down lyrics / copy it of Dua lipa Levitating, read from a file and print it out
     // thread by thread in parallel
     #[test]
     fn test_multi_threaded_queue() {
         let queue = Arc::new(Queue::new("Yo Dua Lipa".to_owned()));
 
-        // TODO: issue here is that the pogram / test runs successfully when a thread performs only
-        // one operation. If there are more than one operations , then i get a use-after-heap-free
-        // error
         let handles = (0..2)
             .map(|j| {
                 let queue_clone = queue.clone();
